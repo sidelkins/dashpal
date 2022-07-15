43daea15aacc10500b486973d864ebb5ba1d2740
sidelkins <sidelkins77@gmail.com>
2022-07-14 20:13:08 -0500

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { onMount } from 'svelte';
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+  import app from './main'
+</script>
+
+<main>
+  <h1>DashPal</h1>
+  <div class="menu">
+    <body>
+      <h2>Google Calendar API</h2>
+
+      <button id="authorize_button">Authorize</button>
+      <button id="signout_button">Sign Out</button>
+  
+    </body>
+  </div>
+</main>
+
+<style>
+
+</style>
